6724a6a0115731feb9a6b18ea43bcc2f818f39b3
Kunall Banerjee <yeskunall96@gmail.com>
2017-10-04 23:35:17 -0400

    create `.zsh/`

---
+++ .zsh/.aliases.zsh
@@ -0,0 +1,18 @@
+# Shortcuts
+alias d="cd D:/"
+alias dl="cd C:/Users/Kunall/Downloads"
+alias dm="cd C:/Users/Kunall/Documents"
+alias dt="cd C:/Users/Kunall/Desktop"
+
+# The aliases below may seem trivial, but they are so useful, you can't even
+# imagine 😅
+alias g='git '
+alias got='git '
+alias get='git '
+alias gti='git '
+
+# Miscellaneous
+alias c='clear'
+alias now='date +"%T"'
+alias weather='curl -4 http://wttr.in/'
+alias week='date +%V'
