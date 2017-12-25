72c125ce30c8fb33c47dcf20a7c911e2b7f554d2
Kunall Banerjee <yeskunall96@gmail.com>
2017-12-25 12:56:34 +0530

    🐍  update `extensions.py` script

--- .vscode/extensions.py
+++ .vscode/extensions.py
@@ -4,10 +4,10 @@ HOME = os.path.expanduser("~")
 VSCODE_EXTENSIONS_PATH = HOME + "/.vscode/extensions"
 EXT_FILE_PATH = os.path.dirname(__file__) + "/extensions.txt"
 
-print(EXT_FILE_PATH)
-
 if not os.path.isfile(EXT_FILE_PATH):
     open(EXT_FILE_PATH, 'w')
+else:
+    os.remove(EXT_FILE_PATH)
 
 for name in os.listdir(VSCODE_EXTENSIONS_PATH):
     abs_path = VSCODE_EXTENSIONS_PATH + "/" + name
