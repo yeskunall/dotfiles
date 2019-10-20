5a9c0a7c3eaf5eaf2964ee6e79bd2b446adba948
Kunall Banerjee <yeskunall96@gmail.com>
2019-10-19 23:50:53 -0400

    Add plugins
     * `zsh-autosuggestions`
    * `zsh-syntax-highlighting`

--- zplug/load.zsh
+++ zplug/load.zsh
@@ -3,6 +3,8 @@ zplug "b4b4r07/enhancd", use:init.sh
 zplug "bezhermoso/zsh-escape-backtick", use:escape-backtick.zsh
 zplug "DarrinTisdale/zsh-aliases-exa", use:zsh-aliases-exa.plugin.zsh
 zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
+zplug "zsh-users/zsh-autosuggestions", defer:2
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 # If no plugins installed are installed, install all of them in parallel
 if ! zplug check --verbose; then
