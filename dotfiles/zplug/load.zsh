98a8d03f290a3e5e1ee28c4def96f0cdb095385c
Kunall Banerjee <hey@kimchiii.space>
2021-11-27 19:42:57 -0500

    (refactor): move to `dotfiles/zplug`

--- dotfiles/zplug/load.zsh
+++ dotfiles/zplug/load.zsh
@@ -1,12 +1,11 @@
 # Command(s)/plugin(s)/theme(s) to install
 zplug "b4b4r07/enhancd", use:init.sh
 zplug "bezhermoso/zsh-escape-backtick", use:escape-backtick.zsh
-zplug "DarrinTisdale/zsh-aliases-exa", use:zsh-aliases-exa.plugin.zsh
-zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
+zplug "lukechilds/zsh-nvm"
 zplug "zsh-users/zsh-autosuggestions", defer:2
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
-# If no plugins installed are installed, install all of them in parallel
+# If no plugins found, install all of them in parallel
 if ! zplug check --verbose; then
   zplug install
 fi
