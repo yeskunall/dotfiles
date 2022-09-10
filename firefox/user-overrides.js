e026ee20f5c6293cbf6a19c46591f5c66cc6f42b
Kunall Banerjee <hey@kimchiii.space>
2022-09-10 17:42:01 -0400

    Custom `user-overrides.js`

---
+++ firefox/user-overrides.js
@@ -0,0 +1,28 @@
+// __       __
+// /  |     /  |
+// __    __  _______  ______   ______          ______  __     __ ______   ______   ______ $$/  ____$$ | ______   _______       __  _______
+// /  |  /  |/       |/      \ /      \ ______ /      \/  \   /  /      \ /      \ /      \/  |/    $$ |/      \ /       |     /  |/       |
+// $$ |  $$ /$$$$$$$//$$$$$$  /$$$$$$  /      /$$$$$$  $$  \ /$$/$$$$$$  /$$$$$$  /$$$$$$  $$ /$$$$$$$ /$$$$$$  /$$$$$$$/      $$//$$$$$$$/
+// $$ |  $$ $$      \$$    $$ $$ |  $$/$$$$$$/$$ |  $$ |$$  /$$/$$    $$ $$ |  $$/$$ |  $$/$$ $$ |  $$ $$    $$ $$      \      /  $$      \
+// $$ \__$$ |$$$$$$  $$$$$$$$/$$ |            $$ \__$$ | $$ $$/ $$$$$$$$/$$ |     $$ |     $$ $$ \__$$ $$$$$$$$/ $$$$$$  __    $$ |$$$$$$  |
+// $$    $$//     $$/$$       $$ |            $$    $$/   $$$/  $$       $$ |     $$ |     $$ $$    $$ $$       /     $$/  |   $$ /     $$/
+// $$$$$$/ $$$$$$$/  $$$$$$$/$$/              $$$$$$/     $/    $$$$$$$/$$/      $$/      $$/ $$$$$$$/ $$$$$$$/$$$$$$$/$$__   $$ $$$$$$$/
+//                               /  \__$$ |
+//                               $$    $$/
+//                                $$$$$$/
+//
+//
+// https://github.com/arkenfox/user.js
+
+// If not set, Firefox pins Google by default
+user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned", "ddg");
+
+// Use CIRA as the default Trusted Recursive Resolver (TRR) for Firefox
+// See https://blog.mozilla.org/mozilla/news/firefox-by-default-dns-over-https-rollout-in-canada/ for more.
+user_pref("doh-rollout.home-region", "CA");
+
+// Do not clear history when Firefox closes
+user_pref("privacy.clearOnShutdown.history", false);
+
+// Do not ask to save logins and passwords for websites
+user_pref("signon.rememberSignons", false);
