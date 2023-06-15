d663220d83ff273e5288bffa2836da2d441df748
Kunall Banerjee <hey@kimchiii.space>
2023-06-15 10:44:21 +0530

    Better syntax highlighting
    But is it faster tho?

--- .config/zplug/load.zsh
+++ .config/zplug/load.zsh
@@ -3,9 +3,9 @@ zplug "b4b4r07/enhancd", use:init.sh
 zplug "bezhermoso/zsh-escape-backtick", use:escape-backtick.zsh
 zplug "lukechilds/zsh-nvm"
 zplug "zsh-users/zsh-autosuggestions", defer:2
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zdharma-continuum/fast-syntax-highlighting", defer:2
 
-# If no plugins found, install all of them in parallel
+# If any plugin is not found, install them in parallel
 if ! zplug check --verbose; then
   zplug install
 fi
