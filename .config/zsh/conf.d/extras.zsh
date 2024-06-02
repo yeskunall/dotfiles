66038303008a417f8e9c13af64a9eb62daf320f7
Kunall Banerjee <hey@kimchiii.space>
2024-06-02 19:30:17 -0400

    feat(zstyle): improve UI/UX

---
+++ .config/zsh/conf.d/extras.zsh
@@ -0,0 +1,14 @@
+# Key bindings
+bindkey -e;
+
+bindkey '^p' history-search-backward;
+bindkey '^n' history-search-forward;
+
+bindkey '^[w' kill-region;
+
+# Completion styling
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
