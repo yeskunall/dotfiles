54c17d93c304bbf48308b0ecc5fdced25d077cf0
Kunall Banerjee <hey@kimchiii.space>
2024-06-02 19:22:58 -0400

    feat(zinit): add plugins

--- .config/zinit/plugins.zsh
+++ .config/zinit/plugins.zsh
@@ -1,9 +1,18 @@
 # Load all plugins (using lucid mode)
 zi wait lucid for  \
+  Aloxaf/fzf-tab  \
   ajeetdsouza/zoxide  \
   bezhermoso/zsh-escape-backtick  \
   lukechilds/zsh-nvm  \
+  make"PREFIX=$ZPFX" tj/git-extras \
   zdharma-continuum/fast-syntax-highlighting  \
+  zsh-users/zsh-autosuggestions \
+  zsh-users/zsh-completions
 
 # Add in Powerlevel10k without wait mode
 zi ice depth=1; zinit light romkatv/powerlevel10k
+
+# Pull in snippets
+zi snippet OMZP::aws # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/aws
+zi snippet OMZP::colored-man-pages # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/colored-man-pages
+zi snippet OMZP::command-not-found # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/command-not-found
