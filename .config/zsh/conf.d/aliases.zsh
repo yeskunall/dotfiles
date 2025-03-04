23e4232048bfd969daccd29edcd7c2c8cbb484c0
Kunall Banerjee <hey@kimchiii.space>
2025-03-04 08:35:26 -0500

    chore: cleanup unused alias(es)

--- .config/zsh/conf.d/aliases.zsh
+++ .config/zsh/conf.d/aliases.zsh
@@ -26,11 +26,6 @@ alias c=" clear";
 # https://github.com/sharkdp/bat
 alias cat="bat";
 
-# Human-readable file sizes by default
-alias df="df -h";
-
-alias du="du -hc";
-
 # Always enable colored `grep` output (`GREP_OPTIONS="--color=auto"` is deprecated)
 alias egrep="egrep --color=auto";
 
@@ -44,25 +39,16 @@ alias glow="glow -p -w 65";
 # The command in the alias will most likely slow down
 # over time as $HISTFILE grows in size
 alias hf="history -E -$(echo `wc -l < $HISTFILE | bc`) | fzf";
-
 alias hfr="history -E -$(echo `wc -l < $HISTFILE | bc`) | fzf --tac";
-
 alias hfh="fzf --history=$HISTFILE --history-size=$(echo `wc -l < $HISTFILE | bc`)";
 
 alias l="eza -lbF --git";
-
 alias la="eza -lbhHigUmuSa --time-style=long-iso --git --color-scale";
-
 alias ll="eza -lbGF --git";
-
 alias llm="eza -lbGF --git --sort=modified";
-
 alias ls="eza";
-
 alias lx="eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale";
-
 alias lt="eza --tree --level=2";
-
 alias lS="eza -1";
 
 # https://github.com/sindresorhus/trash-cli#tip
