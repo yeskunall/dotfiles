60f983e1207647b6c70c029d53c8d17f673a9927
Kunall Banerjee <hey@kimchiii.space>
2025-03-05 11:31:22 -0500

    chore: QoL tweaks

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,9 @@
 vim.g.deprecation_warnings = true
+
 vim.g.lazyvim_picker = "fzf"
+-- Better coop with `fzf-lua`
+vim.env.FZF_DEFAULT_OPTS = ""
+
 vim.g.lazyvim_prettier_needs_config = false
 
 vim.filetype.add {
